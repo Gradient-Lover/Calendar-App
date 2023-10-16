e3e843518aee8c3049afa84777a35f928be79efa
XWeb-CoderX <rileytheangel75@gmail.com>
2023-10-16 20:25:50 +0100

    edited

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Dayz</Text>
+      <View style={styles.calendar}>
+        <View style={styles.day}>
+          <Text style={styles.dayText}>Mon</Text>
+          <View style={styles.event}></View>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.dayText}>Tue</Text>
+          <View style={styles.event}></View>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.dayText}>Wed</Text>
+          <View style={styles.event}></View>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.dayText}>Th</Text>
+          <View style={styles.event}></View>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.dayText}>Fri</Text>
+          <View style={styles.event}></View>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.dayText}>Sat</Text>
+          <View style={styles.event}></View>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.dayText}>Sun</Text>
+          <View style={styles.event}></View>
+        </View>
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Update</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "lightblue",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    color: "purple",
+  },
+  calendar: {
+    flexDirection: "row",
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+    margin: 5,
+  },
+  dayText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "green",
+  },
+  event: {
+    width: 30,
+    height: 30,
+    backgroundColor: "pink",
+    borderRadius: 15,
+    marginTop: 5,
+  },
+  button: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "orange",
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "white",
   },
 });
+
+export default App;
